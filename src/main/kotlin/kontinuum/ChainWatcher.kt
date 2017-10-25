f4d7d0d078d59f56078aecdf53553809d5cdd156
ligi <ligi@ligi.de>
2017-10-25 15:15:05 +0200

    f

--- src/main/kotlin/kontinuum/ChainWatcher.kt
+++ src/main/kotlin/kontinuum/ChainWatcher.kt
@@ -48,52 +48,54 @@ fun processBlockNumber(newBlock: String, statefulChain: StatefulChain) {
 
     val newBlockBigInteger = BigInteger(newBlock.clean0xPrefix(), 16)
     while (lastBlock <= newBlockBigInteger) {
-        lastBlock += BigInteger.ONE
-        statefulChain.lastBlock = "0x" + lastBlock.toString(16)
-
-        processedBlocks.put(statefulChain.chain.name, lastBlock)
-        saveBlockHeights()
 
         val transactions = statefulChain.ethereumRPC.getBlockByNumber("0x" + lastBlock.toString(16))?.transactions
 
-        println("New Block $lastBlock on " + statefulChain.chain.name + " " + transactions?.size + " txs")
+        if (transactions != null) {
+            println("New Block $lastBlock on " + statefulChain.chain.name + " " + transactions.size + " txs")
 
-        transactions?.forEach { tx ->
+            lastBlock += BigInteger.ONE
+            statefulChain.lastBlock = "0x" + lastBlock.toString(16)
+            processedBlocks.put(statefulChain.chain.name, lastBlock)
+            saveBlockHeights()
 
-            tx.to?.let { to ->
-                if (processedTransactions.contains(tx.hash.toLowerCase())) {
-                    println("skipping already processed " + tx.hash)
-                } else {
-                    val kethereumTransaction = tx.toKethereumTransaction()
-                    if (kethereumTransaction.isTokenTransfer()) {
-                        activeIssues.filter { kethereumTransaction.getTokenTransferTo().cleanHex.toLowerCase() == it.address.clean0xPrefix().toLowerCase() }.forEach {
-                            val value = kethereumTransaction.getTokenTransferValue()
-                            val token = tokenMap[statefulChain.chain.networkId.toString()]!![to.clean0xPrefix().toLowerCase()]
-                            if (token != null) {
-                                val scaledValue = BigDecimal(value).divide(BigDecimal(BigInteger("10").pow(BigInteger(token.decimals).toInt())))
+            transactions.forEach { tx ->
 
+                tx.to?.let { to ->
+                    if (processedTransactions.contains(tx.hash.toLowerCase())) {
+                        println("skipping already processed " + tx.hash)
+                    } else {
+                        val kethereumTransaction = tx.toKethereumTransaction()
+                        if (kethereumTransaction.isTokenTransfer()) {
+                            activeIssues.filter { kethereumTransaction.getTokenTransferTo().cleanHex.toLowerCase() == it.address.clean0xPrefix().toLowerCase() }.forEach {
+                                val value = kethereumTransaction.getTokenTransferValue()
+                                val token = tokenMap[statefulChain.chain.networkId.toString()]!![to.clean0xPrefix().toLowerCase()]
+                                if (token != null) {
+                                    val scaledValue = BigDecimal(value).divide(BigDecimal(BigInteger("10").pow(BigInteger(token.decimals).toInt())))
+
+                                    githubInteractor.addIssueComment(it.project, it.issue,
+                                            "Token-transfer [transaction on " + statefulChain.chain.name + "](" + statefulChain.chain.tx_base_url.replace("TXHASH", tx.hash.prepend0xPrefix()) + ")" +
+                                                    " with value " + scaledValue + token.symbol,
+                                            it.installation)
+                                    saveProcessedTX(tx.hash)
+                                } else {
+                                    githubInteractor.addIssueComment(it.project, it.issue,
+                                            "Unknown token-transfer [transaction on " + statefulChain.chain.name + "](" + statefulChain.chain.tx_base_url.replace("TXHASH", tx.hash.prepend0xPrefix()) + ")" +
+                                                    " with value " + value + "<br/>The token must be registered here: [https://github.com/MyEtherWallet/ethereum-lists](https://github.com/MyEtherWallet/ethereum-lists)",
+                                            it.installation)
+                                    saveProcessedTX(tx.hash)
+                                }
+                            }
+
+                        } else {
+                            activeIssues.filter { it.address.toLowerCase().clean0xPrefix() == to.toLowerCase().clean0xPrefix() }.forEach {
+                                val value = BigDecimal(BigInteger(tx.value.clean0xPrefix(), 16)).divide(BigDecimal(ETH_IN_WEI))
                                 githubInteractor.addIssueComment(it.project, it.issue,
-                                        "Token-transfer [transaction on " + statefulChain.chain.name + "](" + statefulChain.chain.tx_base_url.replace("TXHASH", tx.hash.prepend0xPrefix()) + ")" +
-                                                " with value " + scaledValue + token.symbol,
-                                        it.installation)
-                                saveProcessedTX(tx.hash)
-                            } else {
-                                githubInteractor.addIssueComment(it.project, it.issue,
-                                        "Unknown token-transfer [transaction on " + statefulChain.chain.name + "](" + statefulChain.chain.tx_base_url.replace("TXHASH", tx.hash.prepend0xPrefix()) + ")" +
-                                                " with value " + value + "<br/>The token must be registered here: [https://github.com/MyEtherWallet/ethereum-lists](https://github.com/MyEtherWallet/ethereum-lists)",
+                                        "[Transaction on " + statefulChain.chain.name + "](" + statefulChain.chain.tx_base_url.replace("TXHASH", tx.hash.prepend0xPrefix()) + ")" +
+                                                " with value " + value + "ETH",
                                         it.installation)
                                 saveProcessedTX(tx.hash)
                             }
-                        }
-
-                    } else {
-                        activeIssues.filter { it.address.toLowerCase().clean0xPrefix() == to.toLowerCase().clean0xPrefix() }.forEach {
-                            val value = BigDecimal(BigInteger(tx.value.clean0xPrefix(), 16)).divide(BigDecimal(ETH_IN_WEI))
-                            githubInteractor.addIssueComment(it.project, it.issue,
-                                    "[Transaction on " + statefulChain.chain.name + "](" + statefulChain.chain.tx_base_url.replace("TXHASH", tx.hash.prepend0xPrefix()) + ")" +
-                                            " with value " + value + "ETH",
-                                    it.installation)
-                            saveProcessedTX(tx.hash)
                         }
                     }
                 }
